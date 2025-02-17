e82b8e7f2d47306f2702fb87aaffdd12278563f9
eden <ecoding@pm.me>
2025-02-16 16:25:55 -0800

    Upload project thus far

---
+++ Swift-Platformer/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Swift-Platformer
+//
+//  Created by Eden on 2/16/25.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ Swift-PlatformerTests/Swift_PlatformerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_PlatformerTests.swift
+//  Swift-PlatformerTests
+//
+//  Created by Eden on 2/16/25.
+//
+
+import Testing
+@testable import Swift_Platformer
+
+struct Swift_PlatformerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
